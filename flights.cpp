36cad194c5bccac7fd616c7b7cd318d233dcd695
alik15 <ali.mkhalid@gmail.com>
2024-05-11 23:45:29 +0500

    created main and flights file

---
+++ flights.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+class Flights{
+    int id;
+    int seat[500] ={0};
+    std::string source;
+    std::string destination;
+    std::string departureTime;
+    std::string arrivalTime;
+
+    int checkFlightStatus(int flightno ){};
+    // createFlight
+    // bookSeat(id, seatnumber)
+    // editFlight ( flightt no)
+    // seatStatus()
+    // getAllFlights
+    
+
+};
+
